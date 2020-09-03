a1ccbe379faa1f17fd6937cb0ad5c708f924c542
levanhoang <17520509@gm.uit.edu.vn>
2020-09-03 10:06:19 +0700

    update

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
-const express=require('express');
-const app=express();
-const bodyParser=require('body-parser');
-const mongoose=require('mongoose');
-const indexRouter=require('./router/index.router');
-const errorHandle=require('./middle-ware/error-handle');
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const indexRouter = require("./router/index.router");
+const errorHandle = require("./middle-ware/error-handle");
 const expressLayouts = require("express-ejs-layouts");
-const path=require('path');
-var fs = require('fs');
-var upload=require('express-fileupload');
-const cookieParser=require('cookie-parser');
+const path = require("path");
+var fs = require("fs");
+var upload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
 
-
-
-const categoryController=require('./controller/user/category');
+const categoryController = require("./controller/user/category");
 
 // app.use("*", cors());
 // view engine setup
-app.set("views", path.join(__dirname,"views"));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
@@ -29,39 +27,34 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(upload());
 
 //app.set('product_image',path.join(__dirname, '/public/product_image'));
-app.set('layout','admin/layout');
+app.set("layout", "admin/layout");
 app.use(expressLayouts);
 
-app.use('/',(req,res,next)=>{
-    categoryController.getCategory()
-    .then(data=>{
-        app.locals.categorys=data;
+app.use(
+  "/",
+  (req, res, next) => {
+    categoryController
+      .getCategory()
+      .then((data) => {
+        app.locals.categorys = data;
         next();
-    })
-    .catch(err=>{
+      })
+      .catch((err) => {
         console.log(err);
-    })
-},indexRouter);
+      });
+  },
+  indexRouter
+);
 
-
-
-mongoose.connect('mongodb://localhost:27017/rau-cu-qua',(err)=>{
-    if(err)
-    {
-        console.log('can not connect to mongodb');
-    }
-    else
-    {
-        
-        console.log('successful connected to mongodb');
-        
-    }
-})
-
-
-
+mongoose.connect("mongodb://localhost:27017/rau-cu-qua", (err) => {
+  if (err) {
+    console.log("can not connect to mongodb");
+  } else {
+    console.log("successful connected to mongodb");
+  }
+});
 
 app.use(errorHandle);
-app.listen('9000',()=>{
-    console.log('hello ');
-})
+app.listen("9000", () => {
+  console.log("hello ");
+});
